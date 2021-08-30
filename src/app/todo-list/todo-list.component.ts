988d0c794da9eef64ec05c8c234d956184539950
bharath-sampath <bharathram.sampath@gmail.com>
2021-08-31 02:25:46 +0530

    Using cookies to make notes persistant even after refresh

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { FormControl, FormGroup, RequiredValidator, Validators } from '@angular/forms';
 import { ThemeService} from '../theme.service';
 import { moveItemInArray, CdkDragDrop } from "@angular/cdk/drag-drop";
+import { light,dark } from '../theme';
 
 export class noteObject{
   note:string="";
@@ -30,10 +31,32 @@ export class TodoListComponent implements OnInit {
   constructor(private themeService:ThemeService) { }
 
   ngOnInit(): void {
-    this.themeService.setLightTheme();
+    this.notes = JSON.parse(this.getCookie("noteArray"));
+    if (this.getCookie("theme")==="dark")
+    {
+      this.themeService.setActiveTheme(dark);
+    }
+    else
+    {
+      this.themeService.setActiveTheme(light);
+    }
     this.filteredNotes();
   }
+  setCookie(name:string,value:string)
+  {
+    document.cookie= name + "=" + value;
+  }
 
+  getCookie(name:string)
+  {
+    let r=document.cookie.split(';').reduce(function(acc:any, item){
+      let c = item.split('=');
+      c[0] = c[0].replace(' ', '');
+      if (c[0]===name) acc= c[1];
+      return acc;
+  },[]);
+  return r;
+  }
   addNote()
   {
     let newNoteValue:any=this.noteForm.get("newNoteText")?.value;
@@ -45,8 +68,9 @@ export class TodoListComponent implements OnInit {
       this.notes.push(newNote);
       this.noteForm.reset();
     }
+    this.setCookie("noteArray",JSON.stringify(this.notes));
     this.filteredNotes();
-  }
+      }
   filteredNotes()
   {
     if (this.activeFilter) this.filtNotes=this.notes.filter(this.activeNotes);
@@ -56,6 +80,7 @@ export class TodoListComponent implements OnInit {
   onDelete(note:noteObject)
   {
     this.notes.splice(this.notes.indexOf(note),1);
+    this.setCookie("noteArray",JSON.stringify(this.notes));
     this.filteredNotes();
   }
   onToggleTheme()
@@ -63,14 +88,17 @@ export class TodoListComponent implements OnInit {
     if (this.themeService.getActiveTheme().name === 'dark') this.themeService.setLightTheme();
     else this.themeService.setDarkTheme();
 
+    this.setCookie("theme",this.themeService.getActiveTheme().name)
   }
   onComplete(note:noteObject,event:Event)
-  {  
+  {
      this.notes[this.notes.indexOf(note)].noteComplete=(event.target as HTMLInputElement).checked;
+     this.setCookie("noteArray",JSON.stringify(this.notes));
      this.filteredNotes();
   }
   onDrop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.notes, event.previousIndex, event.currentIndex);
+    this.setCookie("noteArray",JSON.stringify(this.notes));
     this.filteredNotes();
   }
   activeNotes(note:noteObject)
@@ -93,9 +121,9 @@ export class TodoListComponent implements OnInit {
   {
     while ( this.notes.findIndex(e => e.noteComplete === true ) >= 0 )
       this.notes.splice( this.notes.findIndex(f => f.noteComplete===true),1);
-    
-    
+
     this.filteredNotes();
+    this.setCookie("noteArray",JSON.stringify(this.notes));
   }
   onSelectAll(event:Event)
   {
@@ -104,26 +132,30 @@ export class TodoListComponent implements OnInit {
   }
   onFilterClick(filter:string)
   {
-    switch (filter) 
+    switch (filter)
     {
       case 'All':
             this.allFilter=true;
             this.activeFilter=false;
-            this.compFilter=false;    
-            this.filteredNotes();        
+            this.compFilter=false;
+            this.filteredNotes();
             break;
       case 'Act':
           this.allFilter=false;
           this.activeFilter=true;
-          this.compFilter=false; 
-          this.filteredNotes();         
+          this.compFilter=false;
+          this.filteredNotes();
           break;
       case 'Comp':
           this.allFilter=false;
           this.activeFilter=false;
           this.compFilter=true;
-          this.filteredNotes();          
+          this.filteredNotes();
           break;
     }
   }
 }
+function reduce() {
+  throw new Error('Function not implemented.');
+}
+
